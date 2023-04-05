9c056531442f58bc3fdd39fe8a40ca3b7400711f
Guilherme Soares <Soares.Guilherme@bcg.com>
2023-04-05 00:50:27 -0300

    adding cors policy

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -15,8 +15,17 @@ namespace API.Extensions
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
 
-            services.AddDbContext<DataContext>(opt => {
+            services.AddDbContext<DataContext>(opt => 
+            {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
             });
 
             services.AddMediatR(typeof(List.Handler));
